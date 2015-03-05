322f675b218ad4bae5ed44e2f2588fa9803ba18b
Altitude Labs <altitudelabs@Altitudes-MacBook-Pro.local>
2015-03-05 11:27:31 +0800

    add fake top bar

--- DBS/ViewController.m
+++ DBS/ViewController.m
@@ -23,9 +23,9 @@
     [super viewDidLoad];
     
     // NAvigation bar
-    UINavigationController* nc = (UINavigationController*)[[[UIApplication sharedApplication] delegate] window].rootViewController;
-    [nc.navigationBar setBarTintColor:[UIColor colorWithRed:206.0/255.0 green:36.0/255.0 blue:43.0/255.0 alpha:1]];
-    nc.navigationBar.translucent = NO;
+//    UINavigationController* nc = (UINavigationController*)[[[UIApplication sharedApplication] delegate] window].rootViewController;
+//    [nc.navigationBar setBarTintColor:[UIColor colorWithRed:206.0/255.0 green:36.0/255.0 blue:43.0/255.0 alpha:1]];
+//    nc.navigationBar.translucent = NO;
     
     
     DraggableViewBackground *draggableBackground = [[DraggableViewBackground alloc]initWithFrame:CGRectMake(0, 0, self.mainView.frame.size.width, self.mainView.frame.size.height)];
